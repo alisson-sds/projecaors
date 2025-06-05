4772ceac2fcd739bd46c08111f5772654a825746
Alisson Silveira <alisson-sds@hotmail.com>
2025-06-05 14:34:50 -0300

    Atualizando miniatura pré-visualização

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,28 @@ export const metadata: Metadata = {
   icons: {
     icon: "/favicon.ico"
   },
+  openGraph: {
+    title: "Projeção Engenharia - Construção e Projetos",
+    description: "Desenvolvemos projetos estruturais e laudos técnicos para Capão da Canoa e região",
+    url: "https://www.projecaors.com.br",
+    siteName: "Projeção Engenharia",
+    images: [
+      {
+        url: "https://www.projecaors.com.br/logo-site.png", // URL absoluta da imagem
+        width: 1200,
+        height: 630,
+        alt: "Projeção Engenharia - Soluções em construção e projetos",
+      }
+    ],
+    locale: "pt_BR",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Projeção Engenharia - Construção e Projetos",
+    description: "Desenvolvemos projetos estruturais e laudos técnicos para Capão da Canoa e região",
+    images: ["https://www.projecaors.com.br/logo-site.png"], // Mesma imagem do Open Graph
+  },
 }
 
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
@@ -16,7 +38,7 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
     "@type": "ProfessionalService",
     "name": "Projeção Engenharia",
     "description": "Construção, Projetos, Laudo Técnico, Obras e Engenharia Civil",
-    "image": "https://www.projecao.com.br/logo.png",
+    "image": "https://www.projecaors.com.br/logo-site.png",
     "address": {
       "@type": "PostalAddress",
       "streetAddress": "Rua Peri, 1777",
@@ -31,7 +53,7 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
       "longitude": "-50.01771833645189"
     },
     "telephone": "+5551990909090",
-    "url": "https://www.projecao.com.br",
+    "url": "https://www.projecaors.com.br",
     "openingHoursSpecification": {
       "@type": "OpeningHoursSpecification",
       "dayOfWeek": ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"],
